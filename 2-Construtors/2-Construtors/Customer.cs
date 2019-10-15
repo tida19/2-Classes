2e58a610a3dc5ee45388855f40bbdfab988cef48
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-15 16:31:11 +0700

    2-Construtors

---
+++ 2-Construtors/2-Construtors/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace _2_Construtors
+{
+    public class Customer
+    {
+        public int id;
+        public string name;
+        public readonly List<Order> Orders = new List<Order>();
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+        public Customer(int ID)
+            : this()
+        {
+            this.id = ID;
+
+        }
+        public Customer(int ID, string Name)
+            : this(ID)
+        {
+            this.id = ID;
+            this.name = Name;
+        }
+        public void Promote()
+        {
+
+        }
+    }
+}
---
+++ 2-Construtors/2-Construtors/Order.cs
@@ -0,0 +1,6 @@
+﻿namespace _2_Construtors
+{
+    public class Order
+    {
+    }
+}
