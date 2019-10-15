ebe6e0178e395fa235752247a20c027b727e348b
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-15 16:17:28 +0700

    1-IntroductionToClasses

---
+++ 1-IntroductionToClasses/1-IntroductionToClasses/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_IntroductionToClasses
+{
+    public class Person
+    {
+        public string Name;
+        public void Introduction(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+        public static Person Parse(string str)
+        {
+            var preson = new Person();
+            preson.Name = str;
+
+            return preson;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var person = Person.Parse("Por");
+
+            
+            person.Introduction("Tida");
+            Console.ReadKey();
+        }
+    }
+}
