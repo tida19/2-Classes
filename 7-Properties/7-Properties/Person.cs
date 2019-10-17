ae551d47c64a51cf482e53f516b73b9ac2084a53
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-17 16:15:02 +0700

    7-Properties

---
+++ 7-Properties/7-Properties/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Usename { get; set; }
+        public DateTime Birthdate { get; private set; }
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var yersr = timeSpan.Days / 365;
+
+                return yersr;
+
+            }
+        }
+    }
+}
