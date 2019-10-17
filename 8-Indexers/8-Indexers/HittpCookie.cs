27f4764adc69eebcbdd4cef8626f1ff102363bfe
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-17 16:21:07 +0700

    8-Indexers

---
+++ 8-Indexers/8-Indexers/HittpCookie.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace index
+{
+    public class HittpCookie
+    {
+        private Dictionary<string, string> _dictionary;
+        public DataTime Expiry { get; set; }
+        public HittpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+        public void SetTime(string key, string value)
+        {
+
+        }
+        public void GetTime(string key)
+        {
+
+        }
+        public string this[string key]
+        {
+            get
+            {
+                return _dictionary[key];
+            }
+            set
+            {
+                _dictionary[key] = value;
+            }
+        }
+        public class DataTime
+        {
+
+        }
+    }
+}
---
+++ 8-Indexers/8-Indexers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HittpCookie();
+            cookie["name"] = "Por";
+            Console.WriteLine(cookie["name"]);
+
+        }
+    }
+}
